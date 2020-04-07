5237f0b54aff525c6cc4d9390b68776c98020f89
Alvin Wang <alvin.wang@uber.com>
2020-04-06 18:07:01 -0700

    check buildables + rename project

--- phabby.js
+++ phabby.js
@@ -12,36 +12,41 @@ const STATUSES = {
     CLOSED: 3,
 };
 
-
+function allBuildablesPassing(buildables) {
+    return Object.values(buildables).every((build) => build.status === 'passed')
+}
 
 async function checkAndLand() {
-    let res;
-    let arcLandPromise;
 
-    exec(`echo '{"ids": [${program.diff}]}' | arc call-conduit differential.query`).then(async (result) => {
+    const diffStatusPromise = exec(`echo '{"ids": [${program.diff}]}' | arc call-conduit differential.query`)
 
-        res = JSON.parse(result.stdout)
+    try {
+        const diffStatus = await diffStatusPromise;
+        let res = JSON.parse(diffStatus.stdout)
 
         const diff = res.response[0];
-        console.log(`D${diff.id}: ${diff.statusName} (${diff.status})`);
-        if (diff.status === STATUSES.ACCEPTED) {
+        const buildables = diff.properties.buildables;
+        const isPassing = allBuildablesPassing(buildables);
+        console.log(`D${diff.id}: ${diff.statusName} (${diff.status}) | ${isPassing ? 'Passing' : 'Failed'}`);
+        if (diff.status === STATUSES.ACCEPTED && isPassing) {
             console.log('Landing');
             clearInterval(phabLoop)
-            arcLandPromise = exec(`git status`)
-            await arcLandPromise;
-            console.log((await arcLandPromise).stdout);
+            let arcLandPromise = exec(`arc land --revision ${program.diff}`)
+            let arcLandRes = await arcLandPromise;
+            console.log(arcLandRes.stdout);
             process.exit(0)
         } else if (diff.status === STATUSES.CLOSED) {
             console.log('Nothing to be done')
+            clearInterval(phabLoop)
             process.exit(0)
         }
-
-    }).catch((error) => {
+    } catch (error) {
         if (error) {
             console.log(`error: ${error.message}`);
             process.exit(1);
         }
-    });
+    }
+
 
 
 }
