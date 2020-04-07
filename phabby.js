e4dd790d419f5d0f12b9ed637b6f88ef643b0b0d
Alvin Wang <alvin.wang@uber.com>
2020-04-06 17:05:39 -0700

    Added error handling

--- phabby.js
+++ phabby.js
@@ -1,47 +1,55 @@
-const { program } = require('commander');
-const { exec } = require('child-process-promise');
+const {program} = require('commander');
+const {exec} = require('child-process-promise');
 
-//echo '{"revisionIDs": [4200647]}' | arc call-conduit differential.querydiffs | jq '.response."13159797".lintStatus'
 program
-  .option('-d, --diff <diffId>', 'Diff Id')
+    .option('-d, --diff <diffId>', 'Diff Id')
 
 
 program.parse(process.argv);
 
-const STATUSES ={
+const STATUSES = {
     ACCEPTED: 2,
+    CLOSED: 3,
 };
 
-let res;
 
-function checkAndLand() {
-    exec(`echo '{"ids": [${program.diff}]}' | arc call-conduit differential.query`,(error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
 
-        res = JSON.parse(stdout)
+async function checkAndLand() {
+    let res;
+    let arcLandPromise;
+
+    exec(`echo '{"ids": [${program.diff}]}' | arc call-conduit differential.query`).then(async (result) => {
+
+        res = JSON.parse(result.stdout)
 
         const diff = res.response[0];
-        console.log(`D${diff.id}: ${diff.statusName}`);
+        console.log(`D${diff.id}: ${diff.statusName} (${diff.status})`);
         if (diff.status === STATUSES.ACCEPTED) {
             console.log('Landing');
             clearInterval(phabLoop)
-            exec(`git status`)
+            arcLandPromise = exec(`git status`)
+            await arcLandPromise;
+            console.log((await arcLandPromise).stdout);
+            process.exit(0)
+        } else if (diff.status === STATUSES.CLOSED) {
+            console.log('Nothing to be done')
+            process.exit(0)
         }
 
+    }).catch((error) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            process.exit(1);
+        }
     });
+
+
 }
 
 checkAndLand()
 
 const phabLoop = setInterval(() => {
-
+    checkAndLand()
 }, 10000);
 
 
