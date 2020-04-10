9c8a438bcf8f873caf3b9d8bf8f0673b6471e0c8
Alvin Wang <alvin7@gmail.com>
2020-04-09 18:29:03 -0700

    Added notifier

--- phaby.js
+++ phaby.js
@@ -1,16 +1,21 @@
-import {version} from './package.json'
+import pkg from './package.json'
 import chalk from 'chalk'
+import updateNotifier from 'update-notifier';
+import notifier from 'node-notifier'
 
 const log = console.log;
 const {program} = require('commander');
 const {exec} = require('child-process-promise');
+
+// Notify if there is a CLI update
+updateNotifier({pkg}).notify();
 
 program
     .requiredOption('-d, --diff <diffId>', 'Diff Id')
     .usage("-d 123454")
     .description('Auto-lands the specified diff when it is accepted and builds are passing.')
 
-program.version(`${version}`)
+program.version(`${pkg.version}`)
 
 program.parse(process.argv);
 
@@ -27,6 +32,7 @@ async function checkAndLand() {
 
     const diffStatusPromise = exec(`echo '{"ids": [${program.diff}]}' | arc call-conduit differential.query`)
 
+
     try {
         const diffStatus = await diffStatusPromise;
         let res = JSON.parse(diffStatus.stdout)
@@ -42,6 +48,10 @@ async function checkAndLand() {
             let arcLandPromise = exec(`arc land --revision ${program.diff}`)
             let arcLandRes = await arcLandPromise;
             console.log(arcLandRes.stdout);
+            notifier.notify({
+                title: `Landed`,
+                message: `Diff D${program.diff} landed ✈️`
+            });
             process.exit(0)
         } else if (diff.status === STATUSES.CLOSED) {
             console.log('Nothing to be done')
